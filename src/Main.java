af4ebbc51ec3528448e605fbaa1095bfe603da9d
derossca <derossca@mail.uc.edu>
2024-02-15 13:18:09 -0500

    imported scanner, declared variables, made do while loop for meters input variable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+//importing scanner
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        //Declaring variables
+        Scanner in = new Scanner(System.in);
+        double meters = 0;
+        double miles = 0;
+        double feet = 0;
+        double inches = 0;
+        String trash = "";
+        boolean done = false;
+
+        //do while loop for the meters variable that requires user input
+        do
+        {
+            System.out.println("Enter the measurement in meters");
+            if(in.hasNextDouble())
+            {
+                meters = in.nextDouble();
+                in.nextLine();
+                if(meters >= 0)
+                {
+                    done = true;
+                }
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("Please enter a valid positive number not " +trash);
+            }
+        }
+        while(!done);
     }
 }
