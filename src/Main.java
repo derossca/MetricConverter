a9f792e7933e2251abd77b43866d8bd374d9fa7f
derossca <derossca@mail.uc.edu>
2024-02-15 13:30:00 -0500

    computed and outputted conversion of meters to feet

--- src/Main.java
+++ src/Main.java
@@ -36,6 +36,10 @@ public class Main {
 
         //computing the conversions of meters to the other measurement variables
         miles = meters * 0.00062137;
-        System.out.println(meters + "meters is " + miles + " miles");
+        System.out.println(meters + " meters is " + miles + " miles");
+        feet = meters * 3.2808399;
+        System.out.println(meters + " meters is " + feet + " feet");
+
+
     }
 }
